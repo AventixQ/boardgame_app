f4fd29409e39a2b5e4450efb75fc9bd011adc6c6
AventixQ <jagoda.anna.janowska@gmail.com>
2023-11-28 18:20:30 +0100

    Setup

---
+++ a.py
@@ -0,0 +1,25 @@
+"""Module A"""
+
+
+class Animal:  # pylint: disable=too-few-public-methods
+    """This is Animal class"""
+
+    def __init__(self, color: int) -> None:
+        """Animal init function
+
+        Args:
+            color (int): color parameter
+        """
+        self.color = color
+
+    def change_colors(self, another_color: int) -> int:
+        """Change color func
+        Args:
+            another_color (int): New color parameter
+
+        Returns:
+            int: Return changed color
+        """
+
+        self.color = another_color
+        return self.color
